9a2698df53b3b8e1e47f0337c0563596b3e7793c
Dunya Kirkali <dunyakirkali@gmail.com>
2024-12-29 22:19:24 +0100

    Increase coverage

--- test/undirected_test.exs
+++ test/undirected_test.exs
@@ -1,30 +1,70 @@
 defmodule UndirectedTest do
   use ExUnit.Case, async: true
 
-  test "reachable in undirected graph" do
-    graph =
-      Graph.new(type: :undirected)
-      |> Graph.add_edges([
-        {:a, :b},
-        {:b, :c},
-        # Separate component
-        {:d, :e}
-      ])
+  describe "reachable in undirected graph" do
+    test "includes all vertices in connected component" do
+      graph =
+        Graph.new(type: :undirected)
+        |> Graph.add_edges([
+          {:a, :b},
+          {:b, :c}
+        ])
 
-    result =
-      Graph.vertices(graph)
-      |> Enum.map(fn vertex ->
-        graph
-        |> Graph.Undirected.reachable([vertex])
-        |> Enum.sort()
-      end)
+      assert Enum.sort(Graph.Undirected.reachable(graph, [:a])) == [:a, :b, :c]
+    end
 
-    assert result == [
-             [:a, :b, :c],
-             [:a, :b, :c],
-             [:a, :b, :c],
-             [:d, :e],
-             [:d, :e]
-           ]
+    test "handles multiple starting vertices" do
+      graph =
+        Graph.new(type: :undirected)
+        |> Graph.add_edges([
+          {:a, :b},
+          {:c, :d}
+        ])
+
+      assert Enum.sort(Graph.Undirected.reachable(graph, [:a, :c])) == [:a, :b, :c, :d]
+    end
+
+    test "returns only starting vertex if isolated" do
+      graph =
+        Graph.new(type: :undirected)
+        |> Graph.add_vertex(:a)
+        |> Graph.add_vertex(:b)
+
+      assert Graph.Undirected.reachable(graph, [:a]) == [:a]
+    end
+
+    test "handles empty starting set" do
+      graph =
+        Graph.new(type: :undirected)
+        |> Graph.add_vertex(:a)
+
+      assert Graph.Undirected.reachable(graph, []) == []
+    end
+
+    test "handles multiple components" do
+      graph =
+        Graph.new(type: :undirected)
+        |> Graph.add_edges([
+          {:a, :b},
+          {:b, :c},
+          {:d, :e}
+        ])
+
+      result =
+        Graph.vertices(graph)
+        |> Enum.map(fn vertex ->
+          graph
+          |> Graph.Undirected.reachable([vertex])
+          |> Enum.sort()
+        end)
+
+      assert result == [
+               [:a, :b, :c],
+               [:a, :b, :c],
+               [:a, :b, :c],
+               [:d, :e],
+               [:d, :e]
+             ]
+    end
   end
 end
